dce5b1a0f425e01c95a90bff1bd72a4b7cc3ff53
Savil Srivastava <676452+savil@users.noreply.github.com>
2024-01-04 12:06:56 -0800

    remove ProfileInstallPackage

--- internal/devbox/util.go
+++ internal/devbox/util.go
@@ -11,6 +11,7 @@ import (
 
 	"github.com/pkg/errors"
 	"go.jetpack.io/devbox/internal/build"
+	"go.jetpack.io/devbox/internal/devpkg"
 	"go.jetpack.io/devbox/internal/integrations/envsec"
 	"go.jetpack.io/devbox/internal/nix/nixprofile"
 
@@ -21,15 +22,19 @@ import (
 // It's used to install applications devbox might need, like process-compose
 // This is an alternative to a global install which would modify a user's
 // environment.
-func (d *Devbox) addDevboxUtilityPackage(ctx context.Context, pkg string) error {
+func (d *Devbox) addDevboxUtilityPackage(ctx context.Context, pkgName string) error {
+	pkg := devpkg.PackageFromStringWithDefaults(pkgName, d.lockfile)
+	installable, err := pkg.Installable()
+	if err != nil {
+		return err
+	}
 	profilePath, err := utilityNixProfilePath()
 	if err != nil {
 		return err
 	}
-
-	return nixprofile.ProfileInstallPackage(ctx, &nixprofile.ProfileInstallPackageArgs{
-		Lockfile:    d.lockfile,
-		Package:     pkg,
+	return nixprofile.ProfileInstall(ctx, &nixprofile.ProfileInstallArgs{
+		Installable: installable,
+		PackageName: pkgName,
 		ProfilePath: profilePath,
 		Writer:      d.stderr,
 	})
